e159e9cac4c1f55dc7fc7fb5b0533594b24fa91c
phrygian <ok.narit@gmail.com>
2019-07-05 14:54:20 +0700

    User template to displaly rss feed

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ RSS_FEEDS = {
 
 @app.route('/')
 def index():
-	return "<h1>Rss Feed News</h1>"
+	return render_template("index.html")
 
 @app.route('/bbc')
 def bbc():
@@ -33,15 +33,10 @@ def fox():
 def get_news(publication):
 	feed = feedparser.parse(RSS_FEEDS[publication])
 	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1>{3} Headlines News</h1>
-			<b>{0}</b> <br />
-			<i>{1}</i> <br />
-			<p>{2}</p> <br />			
-		</body>
-	</html>""".format(first_article.get("title"), first_article.get("published"), \
-		first_article.get("summary"), publication.upper())
+	return render_template("home.html",
+			title=first_article.get("title"),
+			published=first_article.get("published"),
+			summary=first_article.get("summary"))
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
