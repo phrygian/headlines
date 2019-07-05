244db3187b5f822e484cd90ad3d623860e013a14
phrygian <ok.narit@gmail.com>
2019-07-05 13:46:44 +0700

    Add 4 Rss feed news

--- headlines.py
+++ headlines.py
@@ -3,23 +3,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
-SANOOK = "http://rssfeeds.sanook.com/rss/feeds/sanook/news.index.xml"
+RSS_FEEDS = {
+	'bbc' : "http://feeds.bbci.co.uk/news/rss.xml",
+	'cnn' : "http://rss.cnn.com/rss/edition.rss",
+	'posttoday' : "https://www.posttoday.com/rss/src/breakingnews.xml",
+	'sanook' : "http://rssfeeds.sanook.com/rss/feeds/sanook/news.index.xml"
+}
 
 @app.route('/')
-def get_news():
-	feed = feedparser.parse(SANOOK)
+def index():
+	return "<h1>Rss Feed News</h1>"
+
+@app.route('/bbc')
+def bbc():
+	return get_news('bbc')
+
+@app.route('/cnn')
+def cnn():
+	return get_news('cnn')
+
+@app.route('/sanook')
+def sanook():
+	return get_news('sanook')
+
+@app.route('/posttoday')
+def fox():
+	return get_news('posttoday')
+
+def get_news(publication):
+	feed = feedparser.parse(RSS_FEEDS[publication])
 	first_article = feed['entries'][0]
 	return """<html>
 		<body>
-			<h1>BBC Headlines</h1>
+			<h1>{3} Headlines News</h1>
 			<b>{0}</b> <br />
 			<i>{1}</i> <br />
-			<p>{2}</p> <br />
-			<p>{3}</p>
+			<p>{2}</p> <br />			
 		</body>
 	</html>""".format(first_article.get("title"), first_article.get("published"), \
-		first_article.get("summary"), first_article.keys())
+		first_article.get("summary"), publication.upper())
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
